cd5b83953ec8f212859673c626b7f8621db73bb6
Khosraw <khosraw.azizi@gmail.com>
2021-06-20 11:51:16 -0400

    Scripted auto-commit on change (2021-06-20 11:51:16) by gitwatch.sh

--- src/me/khosraw/EvenFibonacciNumbers.java
+++ src/me/khosraw/EvenFibonacciNumbers.java
@@ -1,8 +1,17 @@
 package me.khosraw;
 
+import java.util.Arrays;
+
 public class EvenFibonacciNumbers {
     public static void main (String[] args) {
-        
+        int[] fibo = new int[0];
+
+        for (int i = 0; i <= 4000000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                fibo = Arrays.copyOf(fibo, fibo.length + 1);
+                fibo[fibo.length - 1] = i;
+            }
+        }
 
     }
 }
