b06aa5a9fffb18caa8c13d0f534167b95e340146
Khosraw <khosraw.azizi@gmail.com>
2021-06-20 12:51:37 -0400

    Scripted auto-commit on change (2021-06-20 12:51:37) by gitwatch.sh

--- src/me/khosraw/LargestPrimeFactor.java
+++ src/me/khosraw/LargestPrimeFactor.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 
 public class LargestPrimeFactor {
     public static void main (String[] args) {
-        int[] multiples = new int[0];
+        int[] factors = new int[0];
 
         for (int i = 1; i <= 1000; i++) {
             if (Long.parseLong("600851475143") % i == 0) {
-                multiples = Arrays.copyOf(multiples, multiples.length + 1);
-                multiples[multiples.length - 1] = i;
+                factors = Arrays.copyOf(factors, factors.length + 1);
+                factors[factors.length - 1] = i;
             }
         }
         int result = 0;
         for (int i = 0; i < 467; i++) {
-            result = result + multiples[i];
+            result = result + factors[i];
         }
 
         System.out.println(result);
