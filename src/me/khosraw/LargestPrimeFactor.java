a9bdb48f0d9ca987dc56066e67ed804b3044c4ab
Khosraw <khosraw.azizi@gmail.com>
2021-06-21 18:47:23 -0400

    Scripted auto-commit on change (2021-06-21 18:47:22) by gitwatch.sh

--- src/me/khosraw/LargestPrimeFactor.java
+++ src/me/khosraw/LargestPrimeFactor.java
@@ -3,11 +3,15 @@ package me.khosraw;
 import java.util.Arrays;
 
 public class LargestPrimeFactor {
+    public static void isPrime (int n) {
+        if (n)
+    }
+
     public static void main (String[] args) {
         long[] factors = new long[0];
 
-        for (long i = Long.parseLong("1"); i <= Long.parseLong("600851475143"); i++) {
-            if (Long.parseLong("600851475143") % i == 0 && Long.parseLong("600851475143") % 2 != 0 && Long.parseLong("600851475143") % 3 != 0) {
+        for (long i = Long.parseLong("1"); i <= 600851475143L; i++) {
+            if (600851475143L % i == 0 && Long.parseLong("600851475143") % 2 != 0 && Long.parseLong("600851475143") % 3 != 0) {
                 factors = Arrays.copyOf(factors, factors.length + 1);
                 factors[factors.length - 1] = i;
             }
