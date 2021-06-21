5ec26122cdd810d4680893f39e7a408e7455ccea
Khosraw <khosraw.azizi@gmail.com>
2021-06-21 19:42:30 -0400

    Scripted auto-commit on change (2021-06-21 19:42:30) by gitwatch.sh

--- src/me/khosraw/LargestPalindromeProduct.java
+++ src/me/khosraw/LargestPalindromeProduct.java
@@ -2,6 +2,7 @@ package me.khosraw;
 
 public class LargestPalindromeProduct {
     int[] numDigits = new int[0];
+    int num;
 
     for (int i = 1; i <= 999; i++) {
         for (int j = 1; i <= 999; j++) {
