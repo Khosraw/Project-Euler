09a2bb961678275266591ac21ea127cffa7b93cb
Khosraw <khosraw.azizi@gmail.com>
2021-06-21 19:59:13 -0400

    Scripted auto-commit on change (2021-06-21 19:59:13) by gitwatch.sh

--- src/me/khosraw/LargestPalindromeProduct.java
+++ src/me/khosraw/LargestPalindromeProduct.java
@@ -1,14 +1,31 @@
 package me.khosraw;
 
+import java.util.Arrays;
+
 public class LargestPalindromeProduct {
     public static boolean isPalindrome (int num) {
-        int[] numDigits = new int[0];
+        String stringNum = toString(num);
+        String[] numDigits = new String[0];
 
-        for (int i = 0; )
+        for (int i = 0; i <= stringNum.length(); i++) {
+            numDigits = Arrays.copyOf(numDigits, numDigits.length + 1);
+            numDigits[numDigits.length - 1] = stringNum;
+        }
+
+        String[] newnumDigits = new String[0];
+        for (int i = 0; i <= numDigits.length; i++) {
+            newnumDigits = Arrays.copyOf(newnumDigits, newnumDigits.length + 1);
+            newnumDigits[i] = numDigits[numDigits.length - 1 - i];
+        }
+
+        if ()
+
     }
 
     public static String toString (int num) {
-        String stringNum = num;
+        String stringNum = Integer.toString(num);
+
+        return stringNum;
     }
 
     public static void main (String[] args) {
