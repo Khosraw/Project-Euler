5e8a28b2c266ca0603cdaad97614441305c3cb9b
Khosraw <khosraw.azizi@gmail.com>
2021-06-21 19:19:35 -0400

    Scripted auto-commit on change (2021-06-21 19:19:35) by gitwatch.sh

--- src/me/khosraw/LargestPrimeFactor.java
+++ src/me/khosraw/LargestPrimeFactor.java
@@ -28,7 +28,7 @@ public class LargestPrimeFactor {
                 primeFactors[primeFactors.length - 1] = factor;
             }
         }
-        if (num)
+        if (num > )
         System.out.println(Arrays.toString(primeFactors));
 
     }
