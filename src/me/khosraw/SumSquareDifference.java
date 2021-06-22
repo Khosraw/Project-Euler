00398f0cdcb4ff10786d5bdeed339bcd2ec178c0
Khosraw <khosraw.azizi@gmail.com>
2021-06-22 07:57:52 -0400

    Scripted auto-commit on change (2021-06-22 07:57:52) by gitwatch.sh

--- src/me/khosraw/SumSquareDifference.java
+++ src/me/khosraw/SumSquareDifference.java
@@ -2,11 +2,15 @@ package me.khosraw;
 
 public class SumSquareDifference {
     public static void main (String[] args) {
-        long sumSquare = 0;
+        double sumSquare = 0;
         long squareSum;
 
         for (long i = 1; i <= 100; i++) {
-            sumSquare = sumSquare + Math.pow()
+            sumSquare = sumSquare + Math.pow(i, 2);
+        }
+
+        for (long i = 1; i <= 100; i++) {
+            squareSum = squareSum + Math.pow(i, 2);
         }
 
     }
