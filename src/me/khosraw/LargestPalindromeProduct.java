1adc4318ce4e7d17c92875261aa8bb5a8bbaaded
Khosraw <khosraw.azizi@gmail.com>
2021-06-21 21:21:32 -0400

    Scripted auto-commit on change (2021-06-21 21:21:32) by gitwatch.sh

--- src/me/khosraw/LargestPalindromeProduct.java
+++ src/me/khosraw/LargestPalindromeProduct.java
@@ -3,6 +3,10 @@ package me.khosraw;
 import java.util.Arrays;
 
 public class LargestPalindromeProduct {
+    public static String toString (int num) {
+        return Integer.toString(num);
+    }
+
     public static boolean isPalindrome (int num) {
         String stringNum = toString(num);
 
@@ -14,31 +18,29 @@ public class LargestPalindromeProduct {
             ch[i] = stringNum.charAt(i);
         }
 
+        // Convert ch array to String in numDigits
         String[] numDigits = new String[0];
         for (int i = 0; i < stringNum.length(); i++) {
             numDigits = Arrays.copyOf(numDigits, numDigits.length + 1);
             numDigits[numDigits.length - 1] = String.valueOf(ch[i]);
         }
 
-        // reversing string
+        // Reversing string
         String[] newnumDigits = new String[0];
         for (int i = 0; i < numDigits.length; i++) {
             newnumDigits = Arrays.copyOf(newnumDigits, newnumDigits.length + 1);
             newnumDigits[i] = numDigits[numDigits.length - 1 - i];
         }
 
+        // Turn array into string
         StringBuilder sb = new StringBuilder();
         for (String newnumDigit : newnumDigits) {
             sb.append(newnumDigit);
         }
         String str2 = sb.toString();
+
+        // Compare 2 numbers: Flipped and Non-flipped
         return stringNum.equals(str2);
-
-    }
-
-    public static String toString (int num) {
-
-        return Integer.toString(num);
     }
 
     public static void main (String[] args) {
@@ -59,6 +61,5 @@ public class LargestPalindromeProduct {
             }
         }
         System.out.println(largestNum);
-
     }
 }
