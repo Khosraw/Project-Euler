26cd57abe4aef1e83c200426aaa891b65b3ba48a
Khosraw <khosraw.azizi@gmail.com>
2021-06-22 08:14:28 -0400

    Scripted auto-commit on change (2021-06-22 08:14:28) by gitwatch.sh

--- src/me/khosraw/NumberthPrime.java
+++ src/me/khosraw/NumberthPrime.java
@@ -2,7 +2,11 @@ package me.khosraw;
 
 public class NumberthPrime {
     public static void main (String[] args) {
-        
+        int[] primes = new int[0];
+
+        for (int i = 1; i <= 1000000000; i++) {
+            if (i % 2 != 0 && i % 3 != 0)
+        }
 
     }
 }
