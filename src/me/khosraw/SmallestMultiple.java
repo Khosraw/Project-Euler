e442fd29ad633bf29840d5315d8cf3a89e8d1ad9
Khosraw <khosraw.azizi@gmail.com>
2021-06-22 07:51:22 -0400

    Scripted auto-commit on change (2021-06-22 07:51:22) by gitwatch.sh

--- src/me/khosraw/SmallestMultiple.java
+++ src/me/khosraw/SmallestMultiple.java
@@ -5,8 +5,8 @@ public class SmallestMultiple {
         int counter = 20;
         while (counter <= 2000000000) {
             if (counter % 7 == 0 && counter % 8 == 0 && counter % 9 == 0 && counter % 11 == 0 && counter % 13 == 0 && counter % 14 == 0 && counter % 15 == 0 && counter % 16 == 0 && counter % 17 == 0 && counter % 18 == 0 && counter % 19 == 0 && counter % 20 == 0) {
-                counter = 2000000001;
                 System.out.println(counter + " is the answer!");
+                counter = 2000000001;
             } else {
                 counter = counter + 1;
             }
